dc699f366ae24e1effc97bcb68afa5bc4d76e79d
MetalPizzaCat <catguy228@gmail.com>
2026-04-09 16:17:32 +1200

    update global instructions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::state::NyaState;
+use crate::{instruction::Instruction, state::NyaState};
 
 pub mod instruction;
 pub mod object;
@@ -6,24 +6,12 @@ pub mod state;
 
 fn main() {
     let mut ns = NyaState::new();
-    ns.push_value(0.5);
-    ns.push_value("test");
-    ns.push_value([0, 1, 4, 5]);
-    let n = ns.get_number(-3);
-
-    ns.get_index(-1, 1);
-    let i = ns.get_int(-1);
-
-    let idx = ns.add_constant("blah");
-    ns.get_constant(idx);
-
-    let s = ns.get_string(-4);
-    let s2 = ns.get_string(-1);
-    println!("{s2:?}");
-    println!("{s:?}");
-    println!("{n:?}");
-    println!("{i:?}");
-    ns.pop_stack(4);
-    ns.set_global_direct("test", [0.5]);
-    ns.garbage_collect();
+    ns.add_constant("variable");
+    ns.set_global_direct("variable", "Hello world");
+    let program = vec![
+        Instruction::GetConst(0),
+        Instruction::GetGlobal(0),
+        Instruction::Print,
+    ];
+    ns.run_instructions(&program);
 }
